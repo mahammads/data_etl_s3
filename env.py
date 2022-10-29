83d3d046bad83f55e7f831156bac0e28364564fd
hammads <mahammadjubersarwad@gmail.com>
2022-10-29 15:08:11 +0000

    new changes updated

---
+++ env.py
@@ -0,0 +1,13 @@
+host = ''
+user = ''
+password = ''
+port = 22
+
+s3_bucket_name = ''
+access_key = ''
+secret_key = ''
+region = 'us-east-1'
+local_file_folder = ''
+s3_folder = 'zip_files'
+remote_path = 'document'
+unzip_folder = ''
